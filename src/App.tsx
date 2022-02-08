f74a393655bfb1ce393c60b780cccc0e7015e63e
matosgabriel <gabriel.ifsal@gmail.com>
2022-02-07 23:04:39 -0300

    Fetch of transactions in the fake api and listing of them on the transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,37 @@ import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 // Fake api com miragejs
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        { id: 1, title: 'Freelance', type: 'deposit', category: 'Dev', amount: 5000, createdAt: new Date() },
+        { id: 2, title: 'Freelance2', type: 'deposit', category: 'Dev', amount: 6000, createdAt: new Date() },
+        { id: 3, title: 'Aluguel', type: 'withdraw', category: 'Casa', amount: 2000, createdAt: new Date() }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        { id: 1, title: 'Transaction 1', amount: 400, type: 'deposit', category: 'Food', createdAt: new Date() }
-      ]
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transcation', data);
     });
   }
 });
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { Container } from './styles';
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: 'deposit' | 'withdraw';
+  category: string;
+  createdAt: string;
+}
+
 function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  
   useEffect(() => {
     api.get('http://localhost:3000/api/transactions')
-      .then(response => console.log(response.data));
+      .then(response => setTransactions(response.data.transactions));
   }, []);
   
   return(
@@ -21,24 +32,20 @@ function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className='deposit'>+ R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>07/02/2022</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className='deposit'>+ R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>08/02/2022</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className='withdraw'>- R$2.000</td>
-            <td>Despesa</td>
-            <td>09/02/2022</td>
-          </tr>
+          { transactions.map((transaction) => (
+              <tr key={String(transaction.id)}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)}
+                </td>
+                <td>{transaction.category}</td>
+                <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </Container>
